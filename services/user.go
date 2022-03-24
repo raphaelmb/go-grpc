9fc529adcc2b5e05a7d81612dd5877734b510bcc
Raphael Berger <raphaelmb@gmail.com>
2022-03-24 16:15:03 -0300

    Server stream

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/raphaelmb/go-grpc/pb"
 )
@@ -18,4 +19,36 @@ func NewUserService() *UserService {
 func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
 	fmt.Println(req.Name)
 	return &pb.User{Id: "123", Name: req.GetName(), Email: req.GetEmail()}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User:   &pb.User{Id: "123", Name: req.GetName(), Email: req.GetEmail()},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User:   &pb.User{Id: "123", Name: req.GetName(), Email: req.GetEmail()},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
