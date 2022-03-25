0a32ec980ee2a453c7f44c79d7b4b7826fd35735
Raphael Berger <raphaelmb@gmail.com>
2022-03-25 15:30:05 -0300

    Added client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/raphaelmb/go-grpc/pb"
 	"google.golang.org/grpc"
@@ -18,8 +20,14 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 
-	AddUser(client)
+	// AddUser - unary
+	// AddUser(client)
 
+	// AddUserVerbose - server stream
+	// AddUserVerbose(client)
+
+	// AddUsers - client stream
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -28,6 +36,73 @@ func AddUser(client pb.UserServiceClient) {
 	res, err := client.AddUser(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{Id: "0", Name: "Joao", Email: "j@j.com"}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the message: %v", err)
+		}
+		fmt.Println("Status:", stream.Status)
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		{
+			Id:    "r1",
+			Name:  "Raphael 1",
+			Email: "rpl@rpl.com",
+		},
+		{
+			Id:    "r2",
+			Name:  "Raphael 2",
+			Email: "rpl2@rpl.com",
+		},
+		{
+			Id:    "r3",
+			Name:  "Raphael 3",
+			Email: "rpl3@rpl.com",
+		},
+		{
+			Id:    "r4",
+			Name:  "Raphael 4",
+			Email: "rpl4@rpl.com",
+		},
+		{
+			Id:    "r5",
+			Name:  "Raphael 5",
+			Email: "rpl5@rpl.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
 	}
 
 	fmt.Println(res)
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -19,7 +19,7 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
 	reflection.Register(grpcServer)
-	
+
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
 	}
